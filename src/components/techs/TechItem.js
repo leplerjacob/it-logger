45d7fe06ef001c376ded0696dde42bed7d875be2
jl <jacoblepler91@gmail.com>
2020-08-03 14:15:29 -0400

    Initial and final commit of complete project

---
+++ src/components/techs/TechItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { deleteTech } from "../../actions/techActions";
+import M from "materialize-css/dist/js/materialize.min.js";
+
+const TechItem = ({ tech: { id, firstName, lastName }, deleteTech }) => {
+  return (
+    <li className="collection-item">
+      <div>
+        {firstName} {lastName}
+        <a
+          href="#!"
+          className="secondary-content"
+          onClick={() => deleteTech(id)}
+        >
+          <i className="material-icons grey-text">delete</i>
+        </a>
+      </div>
+    </li>
+  );
+};
+
+TechItem.propTypes = {
+  tech: PropTypes.object.isRequired,
+  deleteTech: PropTypes.func.isRequired,
+};
+
+export default connect(null, { deleteTech })(TechItem);
